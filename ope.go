b369151a254dd252c8f4272f8bc2d6b87f6283d1
direly <luoyi217@126.com>
2019-02-23 21:47:39 +0800

    add opeEncrypt(), opeDecrypt(), test code.

--- ope.go
+++ ope.go
@@ -19,7 +19,7 @@ func printUsage() {
 	fmt.Printf("       %s -h: print help info\n", os.Args[0])
 }
 
-func sampleRange(min uint64, max uint64, coin func() int) uint64 {
+func sampleRange(min int64, max int64, coin func() int) int64 {
 
 	if min > max {
 		return 0
@@ -36,7 +36,7 @@ func sampleRange(min uint64, max uint64, coin func() int) uint64 {
 	return min
 }
 
-func genRandCoin(key []byte, iv uint64) func() int {
+func genRandCoin(key []byte, iv int64) func() int {
 
 	// make sure key length 256 bit
 	if len(key) < 32 {
@@ -46,10 +46,13 @@ func genRandCoin(key []byte, iv uint64) func() int {
 
 	// aes block size 128bit
 	aesBlock, _ := aes.NewCipher(key)
-	p1 := iv
+	p1 := uint64(iv)
 	p2 := uint64(0)
 	plainText := make([]byte, 16)
 	cipherText := make([]byte, 16)
+	binary.LittleEndian.PutUint64(plainText[:8], p1)
+	binary.LittleEndian.PutUint64(plainText[8:], p2)
+	aesBlock.Encrypt(cipherText, plainText)
 	cursor := uint64(0)
 
 	return func() int {
@@ -80,47 +83,114 @@ func genRandCoin(key []byte, iv uint64) func() int {
 
 }
 
-func opeEncrypt(plainRangeL uint64, plainRangeR uint64, cipherRangeL uint64, cipherRangeR uint64, plain uint64, key []byte) uint64 {
+func opeEncrypt(plainRangeL int64, plainRangeR int64, cipherRangeL int64, cipherRangeR int64, plain int64, key []byte) int64 {
+
+	//fmt.Println(plainRangeL, plainRangeR, cipherRangeL, cipherRangeR, plain)
 
 	if plainRangeL > plainRangeR {
 		fmt.Fprintln(os.Stderr, "err: opeEncrypt plainRangeL > plainRangeR")
-		return 0
+		return -1
 	}
 	if cipherRangeL > cipherRangeR {
 		fmt.Fprintln(os.Stderr, "err: opeEncrypt cipherRangeL > cipherRangeR")
-		return 0
+		return -1
 	}
 	if (plainRangeR - plainRangeL) > (cipherRangeR - cipherRangeL) {
 		fmt.Fprintln(os.Stderr, "err: opeEncrypt plain range > cipher range")
-		return 0
+		return -1
 	}
 	if plain > plainRangeR || plain < plainRangeL {
 		fmt.Fprintln(os.Stderr, "err: opeEncrypt plain not in plain range")
-		return 0
+		return -1
 	}
 	if (plainRangeR - plainRangeL) == (cipherRangeR - cipherRangeL) {
-		return cipherRangeR + (plain - plainRangeL)
+		return cipherRangeL + (plain - plainRangeL)
 	}
 
 	cipherMid := (cipherRangeL + cipherRangeR) / 2
 	coin := genRandCoin(key, cipherMid)
 
-	plainSampleRangeL := plainRangeL
-	plainSampleRangeR := plainRangeR
-	if cipherMid-cipherRangeL < plainRangeR-plainRangeL {
-		plainSampleRangeR = plainRangeL + (cipherMid - cipherRangeL)
+	if plainRangeL == plainRangeR {
+		return sampleRange(cipherRangeL, cipherRangeR, coin)
 	}
-	if cipherRangeR-cipherMid < plainRangeR-plainRangeL {
+
+	plainSampleRangeL := int64(0)
+	plainSampleRangeR := int64(0)
+	if plainRangeL+(cipherMid-cipherRangeL) < plainRangeR-1 {
+		plainSampleRangeR = plainRangeL + (cipherMid - cipherRangeL)
+	} else {
+		plainSampleRangeR = plainRangeR - 1
+	}
+	if plainRangeL > plainRangeR-(cipherRangeR-cipherMid) {
+		plainSampleRangeL = plainRangeL
+	} else {
 		plainSampleRangeL = plainRangeR - (cipherRangeR - cipherMid)
 	}
 
-	plainSample := sampleRange(plainSampleRangeL, plainSampleRangeR, coin)
-
-	return 0
+	plainSampleResult := sampleRange(plainSampleRangeL, plainSampleRangeR, coin)
+	if plain <= plainSampleResult {
+		return opeEncrypt(plainRangeL, plainSampleResult, cipherRangeL, cipherMid, plain, key)
+	} else {
+		return opeEncrypt(plainSampleResult+1, plainRangeR, cipherMid+1, cipherRangeR, plain, key)
+	}
 }
 
-func opeDecrypt(plainRangeL uint64, plainRangeR uint64, cipherRangeL uint64, cipherRangeR uint64, plain uint64, key []byte) uint64 {
-	return 0
+func opeDecrypt(plainRangeL int64, plainRangeR int64, cipherRangeL int64, cipherRangeR int64, cipher int64, key []byte) int64 {
+
+	//fmt.Println(plainRangeL, plainRangeR, cipherRangeL, cipherRangeR, cipher)
+
+	if plainRangeL > plainRangeR {
+		fmt.Fprintln(os.Stderr, "err: opeEncrypt plainRangeL > plainRangeR")
+		return -1
+	}
+	if cipherRangeL > cipherRangeR {
+		fmt.Fprintln(os.Stderr, "err: opeEncrypt cipherRangeL > cipherRangeR")
+		return -1
+	}
+	if (plainRangeR - plainRangeL) > (cipherRangeR - cipherRangeL) {
+		fmt.Fprintln(os.Stderr, "err: opeEncrypt plain range > cipher range")
+		return -1
+	}
+	if cipher > cipherRangeR || cipher < cipherRangeL {
+		fmt.Fprintln(os.Stderr, "err: opeEncrypt plain not in plain range")
+		return -1
+	}
+	if (plainRangeR - plainRangeL) == (cipherRangeR - cipherRangeL) {
+		return plainRangeL + (cipher - cipherRangeL)
+	}
+
+	cipherMid := (cipherRangeL + cipherRangeR) / 2
+	coin := genRandCoin(key, cipherMid)
+
+	if plainRangeL == plainRangeR {
+		testResult := sampleRange(cipherRangeL, cipherRangeR, coin)
+		if cipher == testResult {
+			return plainRangeL
+		} else {
+			fmt.Fprintln(os.Stderr, "err: not a valid cipher")
+			return -1
+		}
+	}
+
+	plainSampleRangeL := int64(0)
+	plainSampleRangeR := int64(0)
+	if plainRangeL+(cipherMid-cipherRangeL) < plainRangeR-1 {
+		plainSampleRangeR = plainRangeL + (cipherMid - cipherRangeL)
+	} else {
+		plainSampleRangeR = plainRangeR - 1
+	}
+	if plainRangeL > plainRangeR-(cipherRangeR-cipherMid) {
+		plainSampleRangeL = plainRangeL
+	} else {
+		plainSampleRangeL = plainRangeR - (cipherRangeR - cipherMid)
+	}
+
+	plainSampleResult := sampleRange(plainSampleRangeL, plainSampleRangeR, coin)
+	if cipher <= cipherMid {
+		return opeDecrypt(plainRangeL, plainSampleResult, cipherRangeL, cipherMid, cipher, key)
+	} else {
+		return opeDecrypt(plainSampleResult+1, plainRangeR, cipherMid+1, cipherRangeR, cipher, key)
+	}
 }
 
 func main() {
@@ -134,11 +204,11 @@ func main() {
 
 	switch os.Args[1] {
 	case "-e":
-		plainRangeL := uint64(0)
-		plainRangeR, _ := strconv.ParseUint(os.Args[2], 0, 32)
-		cipherRangeL := uint64(0)
-		cipherRangeR, _ := strconv.ParseUint(os.Args[3], 0, 32)
-		plain, _ := strconv.ParseUint(os.Args[4], 0, 32)
+		plainRangeL := int64(0)
+		plainRangeR, _ := strconv.ParseInt(os.Args[2], 0, 32)
+		cipherRangeL := int64(0)
+		cipherRangeR, _ := strconv.ParseInt(os.Args[3], 0, 32)
+		plain, _ := strconv.ParseInt(os.Args[4], 0, 32)
 		key, _ := hex.DecodeString(os.Args[5])
 
 		if plainRangeR > cipherRangeR {
@@ -149,11 +219,11 @@ func main() {
 		fmt.Println(cipher)
 
 	case "-d":
-		plainRangeL := uint64(0)
-		plainRangeR, _ := strconv.ParseUint(os.Args[2], 0, 32)
-		cipherRangeL := uint64(0)
-		cipherRangeR, _ := strconv.ParseUint(os.Args[3], 0, 32)
-		cipher, _ := strconv.ParseUint(os.Args[4], 0, 32)
+		plainRangeL := int64(0)
+		plainRangeR, _ := strconv.ParseInt(os.Args[2], 0, 32)
+		cipherRangeL := int64(0)
+		cipherRangeR, _ := strconv.ParseInt(os.Args[3], 0, 32)
+		cipher, _ := strconv.ParseInt(os.Args[4], 0, 32)
 		key, _ := hex.DecodeString(os.Args[5])
 
 		if plainRangeR > cipherRangeR {
@@ -173,13 +243,13 @@ func main() {
 		fmt.Println("random key(256bit):")
 		fmt.Println(hex.EncodeToString(key))
 
-	case "-t":
+	case "-t0":
 		key := make([]byte, 32)
 		rand.Read(key)
 		coin := genRandCoin(key, 999)
-		resMap := make(map[uint64]int)
-		for i := 0; i < 1000000; i++ {
-			x := sampleRange(3, 10, coin)
+		resMap := make(map[int64]int)
+		for i := 0; i < 100000; i++ {
+			x := sampleRange(0, 3, coin)
 			_, ok := resMap[x]
 			if ok {
 				resMap[x]++
@@ -188,6 +258,14 @@ func main() {
 			}
 		}
 		fmt.Println(resMap)
+
+	case "-t1":
+		key, _ := hex.DecodeString("3930ed2ff0d28ae5404e1eef2d60643b1135ad3c84da0f0ad99a9695775bd43c")
+		for i := int64(0); i <= 100; i++ {
+			en := opeEncrypt(0, 100, 0, 1000, i, key)
+			de := opeDecrypt(0, 100, 0, 1000, en, key)
+			fmt.Println("test result: ", i, en, de)
+		}
 
 	case "-h":
 		printUsage()
